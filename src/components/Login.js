60a6d8e8de8d5776e2e3f865264ee201ab680033
GeburahGedulah <dominik.heiderer@gmx.at>
2023-04-23 20:11:10 +0200

    smat decisions here....

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import { auth } from '../config/firebase';
 import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState(''); //nice shit
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
